5d4b8a2885ff875da1fa923fbc7c93e621635ecb
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-10-23 11:26:43 +0200

    Added memory maps, changed title and added files

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?= 'The Elephpent in the Room'; ?>
+<?= 'The Elephpent in the Room';
+
+require(__DIR__ . "./memory-cards.php");
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,10 +10,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hitman Roulette</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Memory Game</title>
 </head>
 
 <body>
+    <h1>Hello World</h1>
+    <p>it's 13:45</p>
+
+    <div class="boards">
+        <div class="player-one-board">
+
+            <?php
+
+            foreach ($playingBoard as $rows) {
+                # code...
+            }
+
+            ?>
+
+        </div>
+        <div class="computer-board"></div>
+    </div>
 
 </body>
 
