6978d9a0d59530c67845d3b1094161b27d7ebaf3
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-10-31 15:29:44 +0100

    added favicon and cleaned upp image arrays

--- memory-cards.php
+++ memory-cards.php
@@ -18,58 +18,288 @@ $memoryMapHard = [
     [0, 0, 0, 0, 0]
 ];
 
-$startpageGifs = ['https://media0.giphy.com/media/3oz8xyB3C126ZDDAuk/giphy.gif', 'https://media1.giphy.com/media/3oz8xG0aignBvOhIMU/giphy.gif?cid=790b761100bd40252499e98c55b3a9be1a2f4ca2f146eceb&rid=giphy.gif&ct=g', 'https://media4.giphy.com/media/FPjbHO0jJxGsE/giphy.gif?cid=790b7611cbeb8f448c9c4d9a4190f38cbdb514b90f6b3eab&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/374pcIBVEGb6g/giphy.gif?cid=790b7611fd2e4bde6424b2536f1ee9b341b306756d77e3d3&rid=giphy.gif&ct=g', 'https://media3.giphy.com/media/jSWioe9kzaiMyjx9Km/giphy.gif?cid=790b7611cff772100c495438482e91c8376f18f99545612a&rid=giphy.gif&ct=g', 'https://media2.giphy.com/media/l4FGBYUAKXu5rCN9K/giphy.gif?cid=790b76119c69f06ef7c214e7e88a7395a458d4e176967f9e&rid=giphy.gif&ct=g'];
+$startpageGifs = [
+    "https://media0.giphy.com/media/3oz8xyB3C126ZDDAuk/giphy.gif",
+    "https://media1.giphy.com/media/3oz8xG0aignBvOhIMU/giphy.gif?cid=790b761100bd40252499e98c55b3a9be1a2f4ca2f146eceb&rid=giphy.gif&ct=g",
+    "https://media4.giphy.com/media/FPjbHO0jJxGsE/giphy.gif?cid=790b7611cbeb8f448c9c4d9a4190f38cbdb514b90f6b3eab&rid=giphy.gif&ct=g",
+    "https://media0.giphy.com/media/374pcIBVEGb6g/giphy.gif?cid=790b7611fd2e4bde6424b2536f1ee9b341b306756d77e3d3&rid=giphy.gif&ct=g",
+    "https://media3.giphy.com/media/jSWioe9kzaiMyjx9Km/giphy.gif?cid=790b7611cff772100c495438482e91c8376f18f99545612a&rid=giphy.gif&ct=g",
+    "https://media2.giphy.com/media/l4FGBYUAKXu5rCN9K/giphy.gif?cid=790b76119c69f06ef7c214e7e88a7395a458d4e176967f9e&rid=giphy.gif&ct=g"
+];
 
 $themeHitman = [
-    ['img' => 'images/hitman/47.jpg', 'name' => 'agent 47', 'title' => 'Agent 47', 'description' => ''],
-    ['img' => 'images/hitman/Diana.jpg', 'name' => 'diana burnwood', 'title' => 'Diana Burnwood', 'description' => 'Working as 47 handeler and is one of the best characters'],
-    ['img' => 'images/hitman/Lucas-Grey.jpeg', 'name' => 'lucas grey', 'title' => 'Lucas Grey', 'description' => ''],
-    ['img' => 'images/hitman/Arthur-Edwards.jpeg', 'name' => 'arthur edwards', 'title' => 'Arthur Edwards', 'description' => ''],
-    ['img' => 'images/hitman/Duck.jpeg', 'name' => 'a rubber duck', 'title' => 'Pale Duck', 'description' => ''],
-    ['img' => 'images/hitman/Symbol.jpg', 'name' => 'hitman logo', 'title' => 'Hitman Logo', 'description' => ''],
-    ['img' => 'images/hitman/47-Promo.jpg', 'name' => 'agent 47', 'title' => 'Hitman 2', 'description' => ''],
-    ['img' => 'images/hitman/Pistol.jpeg', 'name' => 'pistol', 'title' => 'Silent Pistol', 'description' => ''],
-    ['img' => 'images/hitman/Poison.jpeg', 'name' => 'poison', 'title' => 'Lethal Poison', 'description' => ''],
-    ['img' => 'images/hitman/Screwdriver.jpeg', 'name' => 'screwdriver', 'title' => 'Screwdriver', 'description' => 'It may seem harmless but is very effective'],
-    ['img' => 'images/hitman/Coin.jpeg', 'name' => 'a coin', 'title' => 'Coin', 'description' => 'Great for distractions and brekaing glas'],
-    ['img' => 'images/hitman/Briefcase.jpeg', 'name' => 'a briefcase', 'title' => 'Briefcase', 'description' => 'Great for carrying around'],
-    ['img' => 'images/hitman/Lockpick.jpeg', 'name' => 'a lockpick', 'title' => 'Lockpick', 'description' => ''],
-    ['img' => 'images/hitman/Fish.jpeg', 'name' => 'a fish', 'title' => 'Fish', 'description' => 'Clearly the superior weapon, just make sure to keep it nice and cold'],
-    ['img' => 'images/hitman/Muffin.jpeg', 'name' => 'a muffin', 'title' => 'Bluberry Muffin', 'description' => 'For when you run out of coins or just want a snack']
+    [
+        "img" => "images/hitman/47.jpg",
+        "name" => "agent 47",
+        "title" => "Agent 47",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Diana.jpg",
+        "name" => "diana burnwood",
+        "title" => "Diana Burnwood",
+        "description" => "Working as 47 handeler and is one of the best characters"
+    ],
+    [
+        "img" => "images/hitman/Lucas-Grey.jpeg",
+        "name" => "lucas grey",
+        "title" => "Lucas Grey",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Arthur-Edwards.jpeg",
+        "name" => "arthur edwards",
+        "title" => "Arthur Edwards",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Duck.jpeg",
+        "name" => "a rubber duck",
+        "title" => "Pale Duck",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Symbol.jpg",
+        "name" => "hitman logo",
+        "title" => "Hitman Logo",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/47-Promo.jpg",
+        "name" => "agent 47",
+        "title" => "Hitman 2",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Pistol.jpeg",
+        "name" => "pistol",
+        "title" => "Silent Pistol",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Poison.jpeg",
+        "name" => "poison",
+        "title" => "Lethal Poison",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Screwdriver.jpeg",
+        "name" => "screwdriver",
+        "title" => "Screwdriver",
+        "description" => "It may seem harmless but is very effective"
+    ],
+    [
+        "img" => "images/hitman/Coin.jpeg",
+        "name" => "a coin",
+        "title" => "Coin",
+        "description" => "Great for distractions and brekaing glas"
+    ],
+    [
+        "img" => "images/hitman/Briefcase.jpeg",
+        "name" => "a briefcase",
+        "title" => "Briefcase",
+        "description" => "Great for carrying around"
+    ],
+    [
+        "img" => "images/hitman/Lockpick.jpeg",
+        "name" => "a lockpick",
+        "title" => "Lockpick",
+        "description" => ""
+    ],
+    [
+        "img" => "images/hitman/Fish.jpeg",
+        "name" => "a fish",
+        "title" => "Fish",
+        "description" => "Clearly the superior weapon, just make sure to keep it nice and cold"
+    ],
+    [
+        "img" => "images/hitman/Muffin.jpeg",
+        "name" => "a muffin",
+        "title" => "Bluberry Muffin",
+        "description" => "For when you run out of coins or just want a snack"
+    ]
 ];
 
 $themeFireEmblem = [
-    ['img' => 'images/fire-emblem/Chrom.png', 'name' => 'Chrom', 'title' => 'Chrom', 'description' => ''],
-    ['img' => 'images/fire-emblem/Robin F.png', 'name' => 'female Robin', 'title' => 'Robin', 'description' => ''],
-    ['img' => 'images/fire-emblem/Frederik.png', 'name' => 'Frederik', 'title' => 'Frederik', 'description' => ''],
-    ['img' => 'images/fire-emblem/Lissa.png', 'name' => 'Lissa', 'title' => 'Lissa', 'description' => ''],
-    ['img' => 'images/fire-emblem/Validar.png', 'name' => 'Validar', 'title' => 'Validar', 'description' => ''],
-    ['img' => 'images/fire-emblem/Aversa.png', 'name' => 'Aversa', 'title' => 'Aversa', 'description' => ''],
-    ['img' => 'images/fire-emblem/Gangrel.png', 'name' => 'Gangrel', 'title' => 'Gangrel', 'description' => ''],
-    ['img' => 'images/fire-emblem/Gaius.png', 'name' => 'Gaius', 'title' => 'Gaius', 'description' => ''],
-    ['img' => 'images/fire-emblem/Panne.png', 'name' => 'Panne', 'title' => 'Panne', 'description' => ''],
-    ['img' => 'images/fire-emblem/Maribelle.png', 'name' => 'Maribelle', 'title' => 'Maribelle', 'description' => ''],
-    ['img' => 'images/fire-emblem/Henry.png', 'name' => 'Henry', 'title' => 'Henry', 'description' => ''],
-    ['img' => 'images/fire-emblem/Lon\'qu.png', 'name' => 'Lon\'qu', 'title' => 'Lon\'qu', 'description' => ''],
-    ['img' => 'images/fire-emblem/Libra.png', 'name' => 'Libra', 'title' => 'Libra', 'description' => ''],
-    ['img' => 'images/fire-emblem/Basilio.png', 'name' => 'Basilio', 'title' => 'Basilio', 'description' => ''],
-    ['img' => 'images/fire-emblem/Flavia.png', 'name' => 'Flavia', 'title' => 'Flavia', 'description' => '']
+    [
+        'img' => 'images/fire-emblem/Chrom.png',
+        'name' => 'Chrom',
+        'title' => 'Chrom',
+        'description' => "Starting Class: \n Lord \n Possible Classes: \n Great Lord, Paladin, etc"
+    ],
+    [
+        'img' => 'images/fire-emblem/Robin F.png',
+        'name' => 'female Robin',
+        'title' => 'Robin',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Frederik.png',
+        'name' => 'Frederik', 'title' => 'Frederik',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Lissa.png',
+        'name' => 'Lissa',
+        'title' => 'Lissa',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Validar.png',
+        'name' => 'Validar',
+        'title' => 'Validar',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Aversa.png',
+        'name' => 'Aversa',
+        'title' => 'Aversa',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Gangrel.png',
+        'name' => 'Gangrel',
+        'title' => 'Gangrel',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Gaius.png',
+        'name' => 'Gaius',
+        'title' => 'Gaius',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Panne.png',
+        'name' => 'Panne',
+        'title' => 'Panne',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Maribelle.png',
+        'name' => 'Maribelle',
+        'title' => 'Maribelle',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Henry.png',
+        'name' => 'Henry',
+        'title' => 'Henry',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Lon\'qu.png',
+        'name' => 'Lon\'qu',
+        'title' => 'Lon\'qu',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Libra.png',
+        'name' => 'Libra',
+        'title' => 'Libra',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Basilio.png',
+        'name' => 'Basilio',
+        'title' => 'Basilio',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ],
+    [
+        'img' => 'images/fire-emblem/Flavia.png',
+        'name' => 'Flavia',
+        'title' => 'Flavia',
+        'description' => "Starting Class: \n \n Possible Classes: \n"
+    ]
 ];
 
 $themePokemon = [
-    ['img' => 'images/pokemon/Infernape.png', 'name' => 'Infernape', 'title' => 'Infernape', 'description' => 'Type: <br> fire, fighting <br> Weak Aggainst <br> Water'],
-    ['img' => 'images/pokemon/Luxray.png', 'name' => 'Luxray', 'title' => 'Luxray', 'description' => ''],
-    ['img' => 'images/pokemon/Roserade.png', 'name' => 'Roserade', 'title' => 'Roserade', 'description' => ''],
-    ['img' => 'images/pokemon/Gyarados.png', 'name' => 'Gyarados', 'title' => 'Gyarados', 'description' => ''],
-    ['img' => 'images/pokemon/Gengar.png', 'name' => 'Gengar', 'title' => 'Gengar', 'description' => ''],
-    ['img' => 'images/pokemon/Purugly.png', 'name' => 'Purugly', 'title' => 'Purugly', 'description' => ''],
-    ['img' => 'images/pokemon/Rapidash.png', 'name' => 'Rapidash', 'title' => 'Rapidash', 'description' => ''],
-    ['img' => 'images/pokemon/Pikachu.png', 'name' => 'Pikachu', 'title' => 'Pikachu', 'description' => ''],
-    ['img' => 'images/pokemon/Garchomp.png', 'name' => 'Garchomp', 'title' => 'Garchomp', 'description' => ''],
-    ['img' => 'images/pokemon/Milotic.png', 'name' => 'Milotic', 'title' => 'Milotic', 'description' => ''],
-    ['img' => 'images/pokemon/Giratina.png', 'name' => 'Giratina', 'title' => 'Giratina', 'description' => ''],
-    ['img' => 'images/pokemon/Houndoom.png', 'name' => 'Houndoom', 'title' => 'Houndoom', 'description' => ''],
-    ['img' => 'images/pokemon/Umbreon.png', 'name' => 'Umbreon', 'title' => 'Umbreon', 'description' => ''],
-    ['img' => 'images/pokemon/Crobat.png', 'name' => 'Crobat', 'title' => 'Crobat', 'description' => ''],
-    ['img' => 'images/pokemon/Altaria.png', 'name' => 'Altaria', 'title' => 'Altaria', 'description' => '']
+    [
+        "img" => "images/pokemon/Infernape.png",
+        "name" => "Infernape",
+        "title" => "Infernape",
+        "description" => "Type: \n fire & fight \n Weak Aggainst: \n Water, Ground, flying & Psychic"
+    ],
+    [
+        "img" => "images/pokemon/Luxray.png",
+        "name" => "Luxray",
+        "title" => "Luxray",
+        "description" => "Type: \n Electric \n Weak Aggainst: \n Ground"
+    ],
+    [
+        "img" => "images/pokemon/Roserade.png",
+        "name" => "Roserade",
+        "title" => "Roserade",
+        "description" => "Type: \n Grass & Poison \n Weak Aggainst: \n Fire, Ice, Flying & Psychic"
+    ],
+    [
+        "img" => "images/pokemon/Gyarados.png",
+        "name" => "Gyarados", "title" => "Gyarados",
+        "description" => "Type: \n Water & Flying \n Weak Aggainst: \n Electric & Rock"
+    ],
+    [
+        "img" => "images/pokemon/Gengar.png",
+        "name" => "Gengar",
+        "title" => "Gengar",
+        "description" => "Type: \n Ghost & Poison \n Weak Aggainst: \n Ground, Psychic & Dark"
+    ],
+    [
+        "img" => "images/pokemon/Purugly.png",
+        "name" => "Purugly",
+        "title" => "Purugly",
+        "description" => "Type: \n Normal \n Weak Aggainst: \n Fight"
+    ],
+    [
+        "img" => "images/pokemon/Rapidash.png",
+        "name" => "Rapidash",
+        "title" => "Rapidash",
+        "description" => "Type: \n Fire \n Weak Aggainst: \n Water, Ground & Rock"
+    ],
+    [
+        "img" => "images/pokemon/Pikachu.png",
+        "name" => "Pikachu",
+        "title" => "Pikachu",
+        "description" => "Type: \n Electric \n Weak Aggainst: \n Ground"
+    ],
+    [
+        "img" => "images/pokemon/Garchomp.png",
+        "name" => "Garchomp",
+        "title" => "Garchomp",
+        "description" => "Type: \n Dragon & Ground \n Weak Aggainst: \n Ice & Dragon"
+    ],
+    [
+        "img" => "images/pokemon/Milotic.png",
+        "name" => "Milotic",
+        "title" => "Milotic",
+        "description" => "Type: \n Water \n Weak Aggainst: \n Electric & Grass"
+    ],
+    [
+        "img" => "images/pokemon/Giratina.png",
+        "name" => "Giratina",
+        "title" => "Giratina",
+        "description" => "Type: \n Ghost & Dragon \n Weak Aggainst: \n Ice, Ghost, Dragon & Dark"
+    ],
+    [
+        "img" => "images/pokemon/Houndoom.png",
+        "name" => "Houndoom",
+        "title" => "Houndoom",
+        "description" => "Type: \n Dark & Fire \n Weak Aggainst: \n Water, Fight, Ground & Rock"
+    ],
+    [
+        "img" => "images/pokemon/Umbreon.png",
+        "name" => "Umbreon",
+        "title" => "Umbreon",
+        "description" => "Type: \n Dark \n Weak Aggainst: \n Fight & Bug"
+    ],
+    [
+        "img" => "images/pokemon/Crobat.png",
+        "name" => "Crobat",
+        "title" => "Crobat",
+        "description" => "Type: \n Poison & Flying \n Weak Aggainst: \n Electric, Ice, Psychic & Rock"
+    ],
+    [
+        "img" => "images/pokemon/Altaria.png",
+        "name" => "Altaria",
+        "title" => "Altaria",
+        "description" => "Type: \n Dragon & Flying \n Weak Aggainst: \n Ice, Rock & Dragon"
+    ]
 ];
